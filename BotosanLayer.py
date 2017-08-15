7c65603c907a0c8dad3ad08d99f54edee78461bd
Frederik Lohner <freddyloher@gmail.com>
2017-08-14 23:26:59 -0400

    Update method and variable names to respect PEP8
    - Add sample unit tests for roll and raza matching.
    - Add raza matching pattern, returns a random name in la raza.

--- BotosanLayer.py
+++ BotosanLayer.py
@@ -19,95 +19,104 @@ class BotosanLayer(YowInterfaceLayer):
         self.logger = logging.getLogger("botosan.logger")
 
     @ProtocolEntityCallback("message")
-    def onMessage(self, messageProtocolEntity):
+    def on_message_received(self, message_protocol_entity):
         # Ack immediately then take a while to respond
-        self.toLower(messageProtocolEntity.ack())
-        self.toLower(messageProtocolEntity.ack(True))
+        """
+        Handles ever message received, if we want to take action on a message we send the message to the lower
+        layers using self.toLower(MessageProtocolEntitity)
+        :param message_protocol_entity: A Message of Parent class: MessageProtocolEntity
+        """
+        self.toLower(message_protocol_entity.ack())
+        self.toLower(message_protocol_entity.ack(True))
 
-        if DateHelper.determineIfBotosanShouldRespond(messageProtocolEntity.getTimestamp()):
-            self.sleepBotosan()
+        if DateHelper.determine_if_botosan_should_respond(message_protocol_entity.getTimestamp()):
+            self.sleep_botosan()
 
-            if messageProtocolEntity.getType() == 'text':
-                if messageProtocolEntity.isGroupMessage():
-                    self.onGroupMessage(messageProtocolEntity)
+            if message_protocol_entity.getType() == 'text':
+                if message_protocol_entity.isGroupMessage():
+                    self.on_group_message(message_protocol_entity)
                 else:
-                    self.onTextMessage(messageProtocolEntity)
-            elif messageProtocolEntity.getType() == 'media':
-                self.onMediaMessage(messageProtocolEntity)
+                    self.on_text_message(message_protocol_entity)
+            elif message_protocol_entity.getType() == 'media':
+                self.on_media_message(message_protocol_entity)
 
-            if messageProtocolEntity.getType() == 'text' and self.regex.doesMatchAPattern(messageProtocolEntity):
-                message_to_send = self.regex.matchPatterns(messageProtocolEntity)
+            if message_protocol_entity.getType() == 'text' and \
+                    self.regex.message_matches_a_pattern(message_protocol_entity):
+                message_to_send = self.regex.generate_message_protocol_for_pattern(message_protocol_entity)
                 if message_to_send is not None:
-                    self.simulateBotosanPreparingAnswer(messageProtocolEntity, messageToSend=message_to_send)
+                    self.logger.info("Sending message %s", message_to_send.getBody())
+                    self.simulate_botosan_preparing_answer(message_protocol_entity, message_to_send=message_to_send)
                 else:
                     self.logger.info("No pattern match.")
 
     @ProtocolEntityCallback("receipt")
-    def onReceipt(self, entity):
+    def on_receipt(self, entity):
         self.toLower(entity.ack())
 
-    def setBotosanOnline(self):
-        self.toLower(AvailablePresenceProtocolEntity())
-
-    def setBotosanDisconnect(self):
-        self.toLower(UnavailablePresenceProtocolEntity())
-
-    def setBotosanPresence(self):
+    def set_botosan_presence(self):
         self.toLower(PresenceProtocolEntity(name="Botosan"))
 
-    def setBotosanTyping(self, message):
+    def set_botosan_online(self):
+        self.toLower(AvailablePresenceProtocolEntity())
+
+    def set_botosan_disconnected(self):
+        self.toLower(UnavailablePresenceProtocolEntity())
+
+    def set_botosan_typing(self, message):
         self.toLower(OutgoingChatstateProtocolEntity(
             OutgoingChatstateProtocolEntity.STATE_TYPING,
             message.getFrom()
         ))
 
-    def stopBotosanTyping(self, message):
+    def stop_botosan_typing(self, message):
         self.toLower(OutgoingChatstateProtocolEntity(
             OutgoingChatstateProtocolEntity.STATE_PAUSED,
             message.getFrom()
         ))
 
-    def simulateBotosanPreparingAnswer(self, messageProtocol, messageToSend):
-        self.setBotosanPresence()
+    def simulate_botosan_preparing_answer(self, message_protocol, message_to_send):
+        self.set_botosan_presence()
 
-        self.setBotosanOnline()
-        self.sleepBotosan()
+        self.set_botosan_online()
+        self.sleep_botosan()
 
-        self.setBotosanTyping(messageProtocol)
-        self.sleepBotosan(minTime=2.0, maxTime=4.0)
+        self.set_botosan_typing(message_protocol)
+        self.sleep_botosan(min_time=2.0, max_time=4.0)
 
-        self.stopBotosanTyping(messageProtocol)
-        self.sleepBotosan()
+        self.stop_botosan_typing(message_protocol)
+        self.sleep_botosan()
 
-        self.toLower(messageToSend)
+        self.toLower(message_to_send)
 
     @staticmethod
-    def sleepBotosan(minTime=0.2, maxTime=0.6):
+    def sleep_botosan(min_time=0.2, max_time=0.6):
         """
         Sleeps botosan for a random amount of time between min and max, defaults to 0.2 and 0.6 if none are provided.
-        :param minTime: Minimum amount of time to sleep
-        :param maxTime: Maximum amount of time to sleep
+        :param min_time: Minimum amount of time to sleep
+        :param max_time: Maximum amount of time to sleep
         """
-        time.sleep(random.uniform(minTime, maxTime))
+        time.sleep(random.uniform(min_time, max_time))
 
-    def onTextMessage(self, messageProtocolEntity):
+    def on_text_message(self, message_protocol_entity):
         """Log the body and phone from which it originated"""
-        self.logger.info("Echoing %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(True)))
+        self.logger.info("Echoing %s to %s" % (message_protocol_entity.getBody(), message_protocol_entity.getFrom(True)))
 
-    def onMediaMessage(self, messageProtocolEntity):
-        if messageProtocolEntity.getMediaType() == "image":
-            self.logger.info("Echoing image %s to %s" % (messageProtocolEntity.url, messageProtocolEntity.getFrom(True)))
+    def on_media_message(self, message_protocol_entity):
+        """Log the image url and phone from which it originated"""
+        if message_protocol_entity.getMediaType() == "image":
+            self.logger.info("Echoing image %s to %s" % (message_protocol_entity.url, message_protocol_entity.getFrom(True)))
 
-        elif messageProtocolEntity.getMediaType() == "location":
+        elif message_protocol_entity.getMediaType() == "location":
             self.logger.info("Echoing location (%s, %s) to %s" % (
-                messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(),
-                messageProtocolEntity.getFrom(False)))
+                message_protocol_entity.getLatitude(), message_protocol_entity.getLongitude(),
+                message_protocol_entity.getFrom(False)))
 
-        elif messageProtocolEntity.getMediaType() == "vcard":
+        elif message_protocol_entity.getMediaType() == "vcard":
             self.logger.info("Echoing vcard (%s, %s) to %s" % (
-                messageProtocolEntity.getName(), messageProtocolEntity.getCardData(),
-                messageProtocolEntity.getFrom(True)))
+                message_protocol_entity.getName(), message_protocol_entity.getCardData(),
+                message_protocol_entity.getFrom(True)))
 
-    def onGroupMessage(self, message):
+    def on_group_message(self, message):
+        """Log the author, participant, user and body of the message"""
         self.logger.info("Group Message: [%s]  ===  [%s]-[%s]\t%s" % (
         message.getAuthor(), message.getParticipant(), message.getFrom(), message.getBody()))
--- Logging.py
+++ Logging.py
@@ -1,5 +1,6 @@
 from slack_logger import SlackHandler, SlackFormatter
 import logging
+import os
 
 
 class BotoSanLogger:
@@ -8,12 +9,13 @@ class BotoSanLogger:
 
     @staticmethod
     def format_logger():
+        hook_url = os.environ["SLACK_HOOK_URL"]
         logging.basicConfig(level=logging.INFO)
         logger = logging.getLogger("botosan.logger")
         logger.setLevel(logging.INFO)
 
         sh = SlackHandler(username='logger', icon_emoji=':robot_face:',
-                          url='https://hooks.slack.com/services/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')
+                          url=hook_url)
         sh.setLevel(logging.INFO)
 
         f = SlackFormatter()
